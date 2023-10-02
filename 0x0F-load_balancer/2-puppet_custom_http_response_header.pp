03db0fb0e28e0041cbbf4849b582d216a7886e7a
Iadolphe007 <iadolphe007@gmail.com>
2023-10-02 17:32:49 +0000

    conf pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,6 +2,9 @@
 exec {'update':
 command => '/usr/bin/apt-get update',
 }
+package { 'nginx':
+ensure => 'present'
+}
 file_line { 'http_header':
 path  => '/etc/nginx/nginx.conf',
 match => 'http {',
