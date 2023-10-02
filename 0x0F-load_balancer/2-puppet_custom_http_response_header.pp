526a719d2ac5fbd3d47fc815ca68518029b3ffa9
Iadolphe007 <iadolphe007@gmail.com>
2023-10-02 17:27:41 +0000

    conf pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,12 @@
 # installin nginx with pupett
-exec {'apt update':
-command => '/usr/bin/apt update',
+exec {'update':
+command => '/usr/bin/apt-get update',
 }
-package { 'nginx':
-ensure  => installed,
-require => Exec[apt update],
+file_line { 'http_header':
+path  => '/etc/nginx/nginx.conf',
+match => 'http {',
+line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-file {'/var/www/html/index.html':
-content =>'Hello world!',
-require => Package['nginx'],
-}
-file { '/var/www/html/404.html':
-content => "Ceci n'est pas une page"
-require => Package['nginx'],
-}
-exec {'nginx':
-ensure  => running,
-require => Package['nginx'],
+exec {'run':
+command => '/usr/sbin/service nginx restart',
 }
