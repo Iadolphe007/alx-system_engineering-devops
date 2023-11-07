48c1056b5fe2a5beb72da1c90fc648d09ef384a1
Iadolphe007 <iadolphe007@gmail.com>
2023-11-07 13:41:55 +0000

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'replace_line':
-  command => "sed -i s/phpp/php/g /var/www/html/wp-settings.php",
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/bin','/usr/bin']
 }
