7d9a26fb091813bac535754f48b4503156096e6d
Iadolphe007 <iadolphe007@gmail.com>
2023-11-07 15:38:28 +0000

    top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """reddit querries"""
+import requests
 
 
 def top_ten(subreddit):
     """prints the titles if the first hot 10 posts"""
-
-    import requests
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0
@@ -15,6 +14,6 @@ def top_ten(subreddit):
     try:
         data = response.get('data').get('children')
         for i in data:
-            print (i.get('data').get('title'))
+            print(i.get('data').get('title'))
     except Exception:
         print('None')
