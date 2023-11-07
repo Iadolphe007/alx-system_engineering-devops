26126d8e5bc87c32d1982d0cf3854e664243bae4
Iadolphe007 <iadolphe007@gmail.com>
2023-11-07 15:10:41 +0000

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,11 @@ def number_of_subscribers(subreddit):
 
     if subreddit is None or subreddit is not isinstance(subreddit, str):
         return 0
+
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My-user-Agent'}
     response = requests.get(url, headers=headers).json
+
     try:
         return response.get('data').get('subscribers')
     except exceptions.RequestException as e:
